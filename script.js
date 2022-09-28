a883fdffbc28500e3932129f05e98176a208b12b
Alt B 2022 <massie.w@gmail.com>
2022-09-28 12:25:44 -0700

    Added computer, player, and game outcome functions

---
+++ script.js
@@ -0,0 +1,23 @@
+playerScore = 0
+computerScore = 0
+
+function computer() {
+    var rockPaperScissors = ["R", "P", "S"];
+    let random = rockPaperScissors[Math.floor(math.random)(rockPaperScissors.length)]
+}
+
+function player() {
+    var rockPaperScissors = ["R", "P", "S"];
+}
+function gameOutcome(computer, player) {
+    if(player === computer) {
+        console.log("draw")
+    }
+    else if(player > computer) {
+        console.log("Player Wins")
+    }
+    else(player < computer) {
+        console.log("Computer Wins")
+    }
+
+}
